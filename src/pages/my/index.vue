f71fe8332946ce7332a19288e2ffffc2880dc8fb
wxmsite <1902726344@qq.com>
2019-04-16 16:22:57 +0800

    update

---
+++ src/pages/my/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="userInfo.avatar">
+    <img class="avatar" :src="userInfo.avatar">
+    <div class="info">
+      <text class="name">{{userInfo.nickname}}</text>
+    </div>
+  </div>
+  <div v-else class="goLogin">
+    <img class="icon" src="/static/images/ic_menu_me_pressed.png">
+    <button
+      v-if="canIUse"
+      open-type="getUserInfo"
+      @getuserinfo="goLogin"
+      class="goLoginBtn"
+    >点击，授权登录~</button>
+  </div>
+</template>
+
+
+<style lang='scss' scoped>
+  @import "./style";
+</style>
