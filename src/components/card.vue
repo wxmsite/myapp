27db39fa401a5f28d8dd1c2cc72aab73fc6f14d5
wxmsite <1902726344@qq.com>
2019-04-17 20:00:11 +0800

    start login

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,19 +1,43 @@
 <template>
-  <div>
-    <p class="card">
+  <div class="card">
+    <div class="img-box">
+      <img class="card-img" :src="imgUrl" alt="" mode="widthFix">
+    </div>
+    <p class="card-text">
       {{text}}
     </p>
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['text']
+  props: {
+    text: String,
+    imgUrl: String
+  }
 }
 </script>
 
-<style>
+<style lang="less">
 .card {
+  display: flex;
   padding: 10px;
+  flex-wrap: wrap;
+  .img-box{
+    width: 100%;
+    height: 80px;
+    border: 1px solid red;
+  }
+  .card-img{
+    width: 100%;
+    height: auto;
+    border-radius: 10px;
+  }
+  .card-text{
+    width: 100%;
+    font: 14px;
+    color: #666666;
+  }
 }
 </style>
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,22 +1,131 @@
 <template>
-  <div v-if="userInfo.avatar">
-    <img class="avatar" :src="userInfo.avatar">
-    <div class="info">
-      <text class="name">{{userInfo.nickname}}</text>
-    </div>
-  </div>
-  <div v-else class="goLogin">
-    <img class="icon" src="/static/images/ic_menu_me_pressed.png">
-    <button
-      v-if="canIUse"
-      open-type="getUserInfo"
-      @getuserinfo="goLogin"
-      class="goLoginBtn"
-    >点击，授权登录~</button>
-  </div>
+  <view>
+    <view class="top">
+      <view class="organ-name">
+        <view v-if="userInfo">{{userInfo.nick_name}}</view>
+        <open-data type="userNickName"></open-data>
+      </view>
+      <view class="avatar">
+        <img v-if="userInfo" class="avavtao-img" :src="userInfo.avatar" alt mode="widthFix">
+        <open-data v-else class="avavtao-img" type="userAvatarUrl"></open-data>
+      </view>
+    </view>
+
+    <button class="btn" open-type="getUserInfo" @getuserinfo="onGotUserInfo">微信授权登录</button>
+
+    <view class="menu">
+      <view class="menu_item">
+        <view>
+          <img class="menu_item_icon" src="../../../static/images/user2.png">
+        </view>
+        <span class="menu_item_content">menu1</span>
+      </view>
+      <view class="menu_item">
+        <view class="menu_item_icon">
+          <img class="menu_item_icon" src="../../../static/images/user2.png">
+        </view>
+        <span class="menu_item_content">menu2</span>
+      </view>
+      <view class="menu_item">
+        <view class="menu_item_icon">
+          <img class="menu_item_icon" src="../../../static/images/user2.png">
+        </view>
+        <span class="menu_item_content">menu3</span>
+      </view>
+    </view>
+    <view>
+      <button class="logout-btn" v-on:click.native="logout">退出登录</button>
+    </view>
+  </view>
 </template>
 
 
+<script>
+import card from "@/components/card";
+
+export default {
+  components: {
+    card
+  },
+  data() {
+    return {
+      userInfo: "",
+      isHidden: false,
+      isPassword: true,
+      logs: [],
+      form: {
+        account: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    init() {
+      this._getUserInfo();
+    },
+    onGotUserInfo(e) {
+      console.log("------>login");
+      wx.setStorageSync("userInfo", e.mp.detail.userInfo);
+      wx.switchTab({
+        url: "../my/main"
+      });
+    }
+  },
+  onShow() {
+    console.log("------>login show");
+    if (wx.getStorageSync("access_token")) {
+      wx.switchTab({
+        url: "../tabBar/course/main"
+      });
+    } else {
+      this.isHidden = true;
+    }
+  },
+  logout() {
+    console.log("------>logout");
+    try {
+      wx.clearStorageSync();
+      this.isHidden = true;
+    } catch (e) {
+      // Do something when catch error
+      wx.switchTab({
+        url: "../tabBar/course/main"
+      });
+      console.log(e);
+    }
+    wx.reLaunch({
+      url: "../../login/main"
+    });
+  },
+  onReady() {
+    console.log("------>login ready");
+  },
+  OnHide() {
+    console.log("onhide");
+    this.isHidden = false;
+  },
+  onUnload() {
+    console.log("onupload");
+    this.isHidden = false;
+  },
+  onShareAppMessage: function(res) {
+    if (res.from === "button") {
+      // 来自页面内转发按钮
+      console.log(res.target);
+    }
+    return {
+      title: "meedu",
+      path: "pages/login/main"
+    };
+  },
+  _getUserInfo(data) {
+    this.$http.user.getUserInfo().then(res => {
+      this.userInfo = res.data;
+    });
+  }
+};
+</script>
+
 <style lang='scss' scoped>
-  @import "./style";
+@import "./style";
 </style>
