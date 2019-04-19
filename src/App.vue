48b08690d30cf269ba71d00b157ff5af8a04c064
wxmsite <1902726344@qq.com>
2019-04-19 20:44:07 +0800

    finish upload file

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
+
+<template>
+  <div id="app">
+    <router-view v-if="isRouterAlive"></router-view>
+  </div>
+</template>
+ 
 <script>
 export default {
-  created () {
-    
-    let logs
-    if (mpvuePlatform === 'my') {
-      logs = mpvue.getStorageSync({key: 'logs'}).data || []
-      logs.unshift(Date.now())
-      mpvue.setStorageSync({
-        key: 'logs',
-        data: logs
-      })
-    } else {
-      logs = mpvue.getStorageSync('logs') || []
-      logs.unshift(Date.now())
-      mpvue.setStorageSync('logs', logs)
+  name: "App",
+  provide() {
+    return {
+      reload: this.reload
+    };
+  },
+  data() {
+    return {
+      isRouterAlive: true
+    };
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(function() {
+        this.isRouterAlive = true;
+      });
     }
   },
-  log () {
-    console.log(`log at:${Date.now()}`)
-  }
-}
-</script>
-
+  components: {}
+};
+</script
 <style>
 .container {
   height: 100%;
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,41 +1,45 @@
 <template>
   <view>
     <view class="top">
-      <view class="organ-name">
-        <view v-if="userInfo">{{userInfo.nick_name}}</view>
-        <open-data type="userNickName"></open-data>
+      <view v-if="status" class="avatar">
+        <open-data class="avatar-img" type="userAvatarUrl"></open-data>
       </view>
-      <view class="avatar">
-        <img v-if="userInfo" class="avavtao-img" :src="userInfo.avatar" alt mode="widthFix">
-        <open-data v-else class="avavtao-img" type="userAvatarUrl"></open-data>
+      <view v-else class="avatar">
+        <img class="avatar-img" src="../../../static/images/avatar.png">
+      </view>
+      <view v-if="status" class="nickname">
+        <button class="btn">
+          <open-data class="nickname-text" type="userNickName"></open-data>
+        </button>
+      </view>
+      <view v-else class="nickname">
+        <button class="btn" open-type="getUserInfo" @getuserinfo="onGotUserInfo">微信登录</button>
       </view>
     </view>
-
-    <button class="btn" open-type="getUserInfo" @getuserinfo="onGotUserInfo">微信授权登录</button>
 
     <view class="menu">
-      <view class="menu_item">
+      <view class="menu_item" @click="uploadFile">
         <view>
-          <img class="menu_item_icon" src="../../../static/images/user2.png">
+          <img class="menu_item_icon" src="../../../static/images/upload-file.svg">
         </view>
-        <span class="menu_item_content">menu1</span>
+        <span class="menu_item_content">上传文件</span>
       </view>
       <view class="menu_item">
         <view class="menu_item_icon">
-          <img class="menu_item_icon" src="../../../static/images/user2.png">
+          <img class="menu_item_icon" src="../../../static/images/explanation.svg">
         </view>
-        <span class="menu_item_content">menu2</span>
+        <span class="menu_item_content">一些说明</span>
       </view>
       <view class="menu_item">
         <view class="menu_item_icon">
-          <img class="menu_item_icon" src="../../../static/images/user2.png">
+          <img class="menu_item_icon" src="../../../static/images/feedback.svg">
         </view>
-        <span class="menu_item_content">menu3</span>
+        <span class="menu_item_content">意见反馈</span>
       </view>
     </view>
-    <view>
-      <button class="logout-btn" v-on:click.native="logout">退出登录</button>
-    </view>
+    <!-- <view>
+      <button class @click="logout">退出登录</button>
+    </view>-->
   </view>
 </template>
 
@@ -50,7 +54,7 @@ export default {
   data() {
     return {
       userInfo: "",
-      isHidden: false,
+      status: false,
       isPassword: true,
       logs: [],
       form: {
@@ -60,53 +64,103 @@ export default {
     };
   },
   methods: {
-    init() {
-      this._getUserInfo();
+    uploadFile() {
+      wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ["original"], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ["album"], // 可以指定来源是相册还是相机，默认二者都有
+        success: function(res) {
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var filePath = res.tempFilePaths[0];
+          wx.showLoading({
+            title: "上传中"
+          });
+          console.log(filePath);
+          const cloudPath =filePath.match(/\.[^.]+.png/)[0];
+          console.log(cloudPath);
+          wx.cloud.init();
+          wx.cloud.uploadFile({
+            cloudPath,
+            filePath,
+            success: res => {
+              console.log("[上传文件] 成功：", res);
+
+              app.globalData.fileID = res.fileID;
+              app.globalData.cloudPath = cloudPath;
+              app.globalData.imagePath = filePath;
+
+              that.setData({
+                fileID: res.fileID,
+                cloudPath: cloudPath,
+                imagePath: filePath
+              });
+
+              console.log(that.data);
+              wx.navigateTo({
+                url: "../storageConsole/storageConsole"
+              });
+            },
+            fail: e => {
+              console.error("[上传文件] 失败：", e);
+              wx.showToast({
+                icon: "none",
+                title: "上传失败"
+              });
+            },
+            complete: () => {
+              wx.hideLoading();
+            }
+          });
+        },
+        fail: e => {
+          console.error(e);
+        }
+      });
+    },
+    logout() {
+      console.log("------>logout");
+      try {
+        wx.removeStorageSync("userInfo");
+      } catch (e) {
+        // Do something when catch error
+        wx.switchTab({
+          url: "../tabBar/course/main"
+        });
+        console.log(e);
+      }
+      this.status = false;
     },
     onGotUserInfo(e) {
       console.log("------>login");
       wx.setStorageSync("userInfo", e.mp.detail.userInfo);
       wx.switchTab({
-        url: "../my/main"
+        url: "../tabBar/index/main"
       });
+      this.status = true;
     }
   },
   onShow() {
     console.log("------>login show");
+
     if (wx.getStorageSync("access_token")) {
       wx.switchTab({
         url: "../tabBar/course/main"
       });
     } else {
-      this.isHidden = true;
+      this.status = true;
     }
   },
-  logout() {
-    console.log("------>logout");
-    try {
-      wx.clearStorageSync();
-      this.isHidden = true;
-    } catch (e) {
-      // Do something when catch error
-      wx.switchTab({
-        url: "../tabBar/course/main"
-      });
-      console.log(e);
-    }
-    wx.reLaunch({
-      url: "../../login/main"
-    });
-  },
+
   onReady() {
     console.log("------>login ready");
   },
   OnHide() {
     console.log("onhide");
-    this.isHidden = false;
+    this.status = false;
   },
   onUnload() {
     console.log("onupload");
-    this.isHidden = false;
+    this.status = false;
   },
   onShareAppMessage: function(res) {
     if (res.from === "button") {
